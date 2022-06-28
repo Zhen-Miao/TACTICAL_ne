68a74bfb7e9b661a58f643447b3f0e2f42d815e3
Zhen-Miao <39448538+Zhen-Miao@users.noreply.github.com>
2022-06-28 19:48:02 -0400

    Update calculate_tissue_vectors.R

--- R/calculate_tissue_vectors.R
+++ R/calculate_tissue_vectors.R
@@ -12,112 +12,96 @@
 #' @export
 calculate_tissue_vectors <- function(snp.annotated.df,tissue_annotation_file,genomic_annotation_file,
                                      ess.annot=NULL,ess.file=NULL){
+
+  ## require some functions 
+  require('data.table')
+  
+  ## read files 
+  tiss.annot.df <- read.table(tissue_annotation_file)
+  # gen.annot.df <- data.table::fread(genomic_annotation_file)
+  
+  ## define some functions
   "%&%" <- function(a,b) paste0(a,b) # just a shortcut for the paste function
   '%>%' <- magrittr::'%>%'
-  tiss.annot.df <- data.table::fread(tissue_annotation_file)
-  gen.annot.df <- data.table::fread(genomic_annotation_file)
   
-  ## ---------------- modified -------------------------
-  ## no ess annotation part 
-  # if (!is.null(ess.annot)){
-  #   ess.df <- data.table::fread(ess.file)
-  # }
-  ## ---------------- end of modified ------------------
+  ## pre-order data frames
+  rr = 1 
+  full.row.df <- snp.annotated.df[rr,]
+  row.df <- full.row.df %>%
+    dplyr::select(.,-one_of("SIGNAL","SNPID","CHR","POS","VALUE"))
   
-  out.df <- c()
-  pb <- txtProgressBar(min=1,max=dim(snp.annotated.df)[1],style=3)
-  for (r in 1:dim(snp.annotated.df)[1]){
-    full.row.df <- snp.annotated.df[r,]
-    row.df <- full.row.df %>% dplyr::select(.,-one_of("SIGNAL","SNPID","CHR","POS","VALUE"))
-    # build annotation matrix for individual SNP
-    name.vec <- names(row.df)
-    tiss.vec <- purrr::map(name.vec,function(s){
-      vec <- strsplit(x=s,split=".",fixed=T)[[1]]
-      ifelse(length(vec)==2,vec[1],NA)
-    }) %>% unlist(.) %>% na.omit(.) %>% as.character(.) %>% unique(.)  %>% sort(.)
-    tiss.annot.vec <- purrr::map(name.vec,function(s){
-      vec <- strsplit(x=s,split=".",fixed=T)[[1]]
-      ifelse(length(vec)==2,vec[2],NA)
-    }) %>% unlist(.) %>% na.omit(.) %>% as.character(.) %>% unique(.) %>% sort(.)
-    gen.annot.vec <- purrr::map(name.vec,function(s){
-      vec <- strsplit(x=s,split=".",fixed=T)[[1]]
-      ifelse(length(vec)==1,vec[1],NA)
-    }) %>% unlist(.) %>% na.omit(.) %>% as.character(.) %>% unique(.) %>% sort(.)
-    annot.vec <- c(tiss.annot.vec,gen.annot.vec)
-    annot.matrix <- matrix(nrow=length(annot.vec),ncol=length(tiss.vec))
-    row.names(annot.matrix) <- annot.vec; colnames(annot.matrix) <- tiss.vec
-    for (i in 1:length(annot.vec)){
-      annot <- annot.vec[i]
-      print.warning <- TRUE
-      for (e in 1:length(tiss.vec)){
-        tiss <- tiss.vec[e]
-        aname <- ifelse(annot %in% tiss.annot.vec, tiss%&%"."%&%annot, annot)
-        wgt <- ifelse(annot %in% tiss.annot.vec,
-                      dplyr::filter(tiss.annot.df,V1==tiss,V2==annot)$V3,
-                      dplyr::filter(gen.annot.df,V1==annot)$V2
-        )
-        
-        ## ---------------- modified -------------------------
-        
-        # if (!is.null(ess.annot)){
-        #   if (aname==ess.annot){
-        #     eval.snp <- dplyr::select(row.df,one_of(aname)) %>% as.numeric(.)
-        #     if (eval.snp==1){
-        #       snp.chr <- full.row.df$CHR; snp.pos <- full.row.df$POS
-        #       sub.df <- dplyr::filter(ess.df,CHR==snp.chr,START<=snp.pos,END>=snp.pos)
-        #       if (dim(sub.df)[1]==1){
-        #         annot.matrix[i,e] <- (dplyr::select(sub.df,one_of(tiss)) %>% as.numeric(.)) * wgt
-        #       } else if (dim(sub.df)[1]==0){
-        #         if (print.warning==TRUE){
-        #           write("\nWARNING: SNP " %&% full.row.df$SNPID %&% " at SIGNAL " %&% full.row.df$SIGNAL %&%
-        #                   " maps to annotation " %&% aname %&%
-        #                   " but SNP does not map to feature in provided specificity file, please inspect",stdout())
-        #           print.warning<-FALSE
-        #         }
-        #         # when tissue specificity information is missing, defaults to equal value for each tissue
-        #         default.val <- 1 / length(tiss.vec)
-        #         annot.matrix[i,e] <- default.val * wgt
-        #       } else if (dim(sub.df)[1]>1){
-        #         if (print.warning==TRUE){
-        #           write("\nWARNING: SNP " %&% full.row.df$SNPID %&% " at SIGNAL " %&% full.row.df$SIGNAL %&%
-        #                   " maps to annotation " %&% aname %&%
-        #                   " but SNP maps to multiple features in provided specificity file, please inspect",stdout())
-        #           print.warning<-FALSE
-        #         }
-        #         # when tissue specificity information is ambiguous (i.e. snp maps to multiple features),
-        #         # defaults to equal value for each tissue
-        #         default.val <- 1 / length(tiss.vec)
-        #         annot.matrix[i,e] <- default.val * wgt
-        #       } else{
-        #         write("\nError linking annotated SNP to specificity file, please inspect",stderr())
-        #       }
-        #     } else{
-        #       annot.matrix[i,e] <- (dplyr::select(row.df,one_of(aname)) %>% as.numeric(.)) * wgt
-        #     }
-        #   } else{
-        #     annot.matrix[i,e] <- (dplyr::select(row.df,one_of(aname)) %>% as.numeric(.)) * wgt
-        #   }
-        # }
-        
-          annot.matrix[i,e] <- (dplyr::select(row.df,one_of(aname)) %>% as.numeric(.)) * wgt
-        
-        
+  ## get all the tissue types
+  name.vec <- names(row.df)
+  tiss.vec <- purrr::map(name.vec,function(s){
+    vec <- strsplit(x=s,split=".",fixed=T)[[1]]
+    ifelse(length(vec)==2,vec[1],NA)
+  }) %>% unlist(.) %>% na.omit(.) %>% as.character(.) %>% unique(.)  %>% sort(.)
+  
+  ## get all the annotation types
+  tiss.annot.vec <- purrr::map(name.vec,function(s){
+    vec <- strsplit(x=s,split=".",fixed=T)[[1]]
+    ifelse(length(vec)==2,vec[2],NA)
+  }) %>% unlist(.) %>% na.omit(.) %>% as.character(.) %>% unique(.) %>% sort(.)
+  
+  ## get all the genome annotation types (only one type, coding)
+  gen.annot.vec <- purrr::map(name.vec,function(s){
+    vec <- strsplit(x=s,split=".",fixed=T)[[1]]
+    ifelse(length(vec)==1,vec[1],NA)
+  }) %>% unlist(.) %>% na.omit(.) %>% as.character(.) %>% unique(.) %>% sort(.)
+  
+  ## step 0. remove the coding part from the data frame 
+  snp.annotated.df <- snp.annotated.df[,coding:=NULL]
+  
+  ## step 1. reorder the annotations 
+  ta = vector(length = length(tiss.annot.vec) * length(tiss.vec)  )
+  for (i in 1:length(tiss.annot.vec)){
+    annot_wo_e <- tiss.annot.vec[i]
+    print.warning <- TRUE
+    for (e in 1:length(tiss.vec)){
+      tiss <- tiss.vec[e]
+      ta[(i-1)*length(tiss.vec) + e]  = tiss%&%"."%&%annot_wo_e
+    }
+  }
+  
+  ta_snp <-  append( c("SIGNAL","SNPID","CHR","POS","VALUE"),ta)
+  setcolorder(snp.annotated.df, ta_snp)
+  
+  rownames(tiss.annot.df) = tiss.annot.df$V1%&%"."%&%tiss.annot.df$V2
+  tiss.annot.df = tiss.annot.df[ta,]
+  
+  ## step 1.5 calculate some values
+  n_tissues <- length(tiss.vec)
+  n_snp <- dim(snp.annotated.df)[1]
+  n_annos <- dim(snp.annotated.df)[2] ## c("SIGNAL","SNPID","CHR","POS","VALUE")
+  
+  ### step 2. restructure the matrices by snp 
+  
+  ## ---- this matrix is only constructed once -------
+  annot.matrix_all <- matrix(as.numeric(tiss.annot.df$V3), nrow = n_tissues)
+  # rownames(annot.matrix_all) <- tiss.vec
+  # colnames(annot.matrix_all) <- tiss.annot.vec
+  
+  ## use tiss.wgts matrix to store all the tissue weights 
+  tiss.wgts <- matrix(nrow = n_snp,ncol = n_tissues)
+  colnames(tiss.wgts) <- tiss.vec
+  snp_trunc_df <- as.matrix(as.data.frame(snp.annotated.df)[,6:n_annos])
+  # snp_trunc_df <- as.numeric(snp_trunc_df)
+  
+  ## loop for each SNP, note, no nested loop
+    for (r in 1:n_snp){
+    wgt <- matrix(snp_trunc_df[r,], nrow = n_tissues)
+    annot.matrix <- annot.matrix_all * wgt
+    
+      if (sum(annot.matrix) > 0){
+        tiss.wgts[r,] <- rowSums(annot.matrix) / sum(annot.matrix)
+      } else{
+        tiss.wgts[r,] <- rep(0,n_tissues)
       }
     }
-    # Sum and scale annotation vectors
-    if (sum(colSums(annot.matrix)) > 0){
-      tiss.wgts <- colSums(annot.matrix) / sum(colSums(annot.matrix))
-    } else{
-      tiss.wgts <- rep(0,length(tiss.vec))
-      names(tiss.wgts) <- tiss.vec
-    }
-    # Output tissue scores 
-    tiss.vector <- tiss.wgts
-    sub.df <- dplyr::select(full.row.df,one_of("SIGNAL","SNPID","CHR","POS","VALUE"))
-    build.df <- cbind(sub.df,(as.data.frame(tiss.vector) %>% t(.)))
-    out.df <- rbind(out.df,build.df)
-    setTxtProgressBar(pb,r)
-  }
-  return(out.df)
+  
+  ## to be consistent to the original output 
+  out.df <- cbind(as.data.frame(snp.annotated.df)[,1:5], as.data.frame(tiss.wgts))
+  return(out.df) 
+
 }
 
